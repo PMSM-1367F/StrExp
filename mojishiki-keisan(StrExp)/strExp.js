4f3af7573e8dcf573e8f4926b9d814530864c0e3
KQ1367 <pmsm1372vvvf@gmail.com>
2025-04-28 18:28:19 +0900

    strExp.js beta 2.2.5

--- mojishiki-keisan(StrExp)/strExp.js
+++ mojishiki-keisan(StrExp)/strExp.js
@@ -10,8 +10,8 @@ function factorial(num = 0){
 class StrExp{
     /**
      * StrExpオブジェクトを生成
+     * @constructor
      * @param {string} strExp 文字列で書かれた文字式
-     * @returns {object} StrExpオブジェクト
      */
     constructor(strExp = '0', useBigInt = false){
         const REG = StrExp.PATTERN;
@@ -20,37 +20,55 @@ class StrExp{
         }
         let arrayIndex = 0;
         let currentStr1 = ''; // multiedNum
-        let currentStr2 = ''; // multiedStr
+        let currentStr2 = {
+            method: null,
+            string: ''
+        }; // multiedStr
         let currentStr3 = ''; // pow
         let isGeningPow = false;
+        const isNum = function(val){
+            return Number.isNaN(Number(val))
+        }
+        const isSign = function(val){
+            return val === '+' || val === '-';
+        }
         for(let i = 0; i < strExp.length; i++){
             let cSt = strExp.at(i);
-            const isNum = function(val){
-                return Number.isNaN(Number(val))
+            if(cSt === '#'){
+                let escapeString = '';
+                for(let j = 0; j < 2; j++){
+                    i++;
+                    cSt = strExp.at(i);
+                    escapeString += cSt;
+                }
+                console.log(escapeString);
+                const ESCAPE = StrExp.#getEscape(escapeString);
+                currentStr2.method = ESCAPE;
+                continue;
             }
-            const isSign = function(val){
-                return val === '+' || val === '-';
-            }
+            // 係数なら
             if ((!isNum(cSt) || cSt === '.') && !isGeningPow) {
                 currentStr1 += cSt;
+            // 指数なら
             } else if(cSt === '^' || (isGeningPow && (!currentStr3 && isSign(cSt)))){
                 !isGeningPow ? i++ : null /*何もしない*/;
                 cSt = strExp.at(i);
                 currentStr3 += isNum(cSt) ? '' : cSt;
                 isGeningPow = true;
+            // 文字なら
             } else if (!isSign(cSt)) {
-                currentStr2 += cSt;
+                currentStr2.string += cSt;
             }
             if(isSign(cSt) && (i !== 0 || isGeningPow) || i === strExp.length - 1){
                 currentStr3 ||= '1';
                 currentStr1 = (useBigInt ? BigInt : Number)(currentStr1);
                 currentStr3 = (useBigInt ? BigInt : Number)(currentStr3);
                 this[arrayIndex] = new StrExp.#Exp(currentStr1, currentStr2, currentStr3);
-                if(this[arrayIndex].multiedStr === ''){
+                if(this[arrayIndex].multiedStr.string === ''){
                     this[arrayIndex].pow = useBigInt ? 0n : 0;
                 }
                 arrayIndex++;
-                [currentStr1, currentStr2, currentStr3] = ['', '', ''];
+                [currentStr1, currentStr2, currentStr3] = ['', {method: null, string: ''}, ''];
                 isGeningPow = false;
             }
             if(cSt === '-'){
@@ -60,7 +78,7 @@ class StrExp{
         /**
          * @type {string}
          */
-        this.EXP_ID = (function(){
+        this[StrExp.getExpIdKey] = (function(){
             const ID_STR = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
             let id = '';
             outerLoop: for(let j = 0; j < 4; j++){
@@ -74,6 +92,7 @@ class StrExp{
             }
             return id;
         })();
+        this.useBigInt = useBigInt;
         this.length = arrayIndex;
     }
     static #Exp = class Exp{
@@ -96,6 +115,49 @@ class StrExp{
         const result = StrExp.#isVaildVal(objExp);
         if(!result){
             throw new TypeError(`arguments is not StrExp object`);
+        }
+    }
+    static #getEscape(str){
+        switch(str){
+            case 'as': {
+                return 'asin';
+            }
+            case 'ac': {
+                return 'acos';
+            }
+            case 'at': {
+                return 'atan';
+            }
+            case 's0': {
+                return 'sin';
+            }
+            case 'c0': {
+                return 'cos';
+            }
+            case 't0': {
+                return 'tan';
+            }
+            case 'sh': {
+                return 'sinh';
+            }
+            case 'ch': {
+                return 'cosh';
+            }
+            case 'th': {
+                return 'tanh';
+            }
+            case 'fa': {
+                return 'factorial';
+            }
+            case 'lg': {
+                return 'log10';
+            }
+            case 'in': {
+                return 'log';
+            }
+            default: {
+                throw new SyntaxError('invaild escape');
+            }
         }
     }
     /**
@@ -161,9 +223,19 @@ class StrExp{
      */
     static invert(objExp){
         StrExp.#throwErrorIfInvaild(objExp);
-        for(const exp of objExp){
-            exp.multiedNum = -(exp.multiedNum);
+        const strExpObj = Object.create(this.prototype);
+        for(const n in objExp){
+            if(Number.isNaN(Number(n))){
+                break;
+            }
+            const expObj = new StrExp.#Exp(
+                -(objExp[n].multiedNum), 
+                objExp[n].multiedStr, 
+                objExp[n].pow
+            );
+            strExpObj[n] = expObj;
         }
+        return strExpObj;
     }
     /**
      * 足し算
@@ -174,13 +246,13 @@ class StrExp{
     static positive(objExp1, objExp2){
         StrExp.#throwErrorIfInvaild(objExp1);
         StrExp.#throwErrorIfInvaild(objExp2);
-        let resultList = [new StrExp('0')];
+        let resultList = [new this('0')];
         const mergedExp = [...objExp1, ...objExp2];
-        const mostExp = StrExp.theMostExp(mergedExp);
+        const mostExp = this.theMostExp(mergedExp);
         const worstExp = StrExp.#theWorstExp(mergedExp);
-        const MULTIED_STR = StrExp.pickExp(objExp1, 1)[0].multiedStr;
+        const MULTIED_STR = this.pickExp(objExp1, 1)[0].multiedStr.string;
         for(let i = 0; i <= mostExp; i++){
-            resultList[i] = StrExp.pickExp(mergedExp, i);
+            resultList[i] = this.pickExp(mergedExp, i);
         }
         let exp = '';
         for(let i = 0; i < resultList.length; i++){
@@ -206,7 +278,7 @@ class StrExp{
                 }
             }
         }
-        return new StrExp(exp);
+        return new this(exp);
     }
     /**
      * 引き算
@@ -218,8 +290,8 @@ class StrExp{
         StrExp.#throwErrorIfInvaild(objExp1);
         StrExp.#throwErrorIfInvaild(objExp2);
         const inverted = JSON.parse(JSON.stringify([...objExp2]));
-        StrExp.invert(inverted);
-        return StrExp.positive(objExp1, inverted);
+        this.invert(inverted);
+        return this.positive(objExp1, inverted);
     }
     /**
      * 掛け算
@@ -231,6 +303,7 @@ class StrExp{
         StrExp.#throwErrorIfInvaild(objExp1);
         StrExp.#throwErrorIfInvaild(objExp2);
         let results = '';
+        const MULTIED_STR = this.pickExp(objExp1, 1)[0].multiedStr.string;
         for(let i = 0; i < objExp1.length; i++){
             for(let j = 0; j < objExp2.length; j++){
                 const [float1, float2] = [
@@ -253,13 +326,13 @@ class StrExp{
                         break zeroOrOne2;
                     }
                     default: {
-                        results += `${multiResult}x^${powResult}`;
+                        results += `${multiResult}${MULTIED_STR}^${powResult}`;
                     }
                 }
             }
         }
-        let result = new StrExp(results);
-        result = StrExp.positive(result, new StrExp('0'));
+        let result = new this(results);
+        result = this.positive(result, new this('0'));
         return result;
     }
     /**
@@ -274,24 +347,23 @@ class StrExp{
         if(objExp2.toString().match('^0')){
             throw new Error('divided by 0');
         }
-        console.log(StrExp.theMostExp(objExp1), StrExp.theMostExp(objExp2))
-        const EXP1 = StrExp.theMostExp(objExp1) - StrExp.theMostExp(objExp2);
+        const EXP1 = this.theMostExp(objExp1) - this.theMostExp(objExp2);
         if(EXP1 < 0){
             throw new RangeError('objExp2 exp is bigger than objExp1 exp');
         }
-        const MULTIED_STR = StrExp.pickExp(objExp1, 1)[0].multiedStr;
+        const MULTIED_STR = this.pickExp(objExp1, 1)[0].multiedStr.string;
         let multiList = [], expStr = '';
         const EXP2 = StrExp.#theWorstExp([...objExp1, ...objExp2]);
-        const EXP3 = StrExp.theMostExp([...objExp1, ...objExp2]);
+        const EXP3 = this.theMostExp([...objExp1, ...objExp2]);
         for(let i = EXP1, 
             repeat = EXP3 - EXP2,
-            most1 = StrExp.theMostExp(objExp1), 
-            most2 = StrExp.theMostExp(objExp2); 
+            most1 = this.theMostExp(objExp1), 
+            most2 = this.theMostExp(objExp2); 
             repeat > 0;
             i--, 
             repeat--,
-            most1 = StrExp.theMostExp(objExp1),
-            most2 = StrExp.theMostExp(objExp2)
+            most1 = this.theMostExp(objExp1),
+            most2 = this.theMostExp(objExp2)
         ){
             let numList = [];
             for(let j = 0; j < Math.abs(EXP1) + 2; j++){
@@ -301,7 +373,7 @@ class StrExp{
                     {[MULTIED_STR]: j + 1}, j + 1
                 );
             }
-            console.log(numList)
+            console.debug(numList);
             multiList[i] = (
                 i === 0 ? 
                 numList[0] : 
@@ -320,20 +392,25 @@ class StrExp{
             } else {
                 expStr += `${multiList[i]}${MULTIED_STR}^${i}`;
             }
-            objExp1 = StrExp.negative(objExp1, StrExp.pickExp(objExp1, most1));
-            objExp2 = StrExp.negative(objExp2, StrExp.pickExp(objExp2, most2));
+            objExp1 = this.negative(objExp1, this.pickExp(objExp1, most1));
+            objExp2 = this.negative(objExp2, this.pickExp(objExp2, most2));
         }
-        const result = new StrExp(expStr);
+        const result = new this(expStr);
         return result;
     }
-    static from(iteratable){
-        const StrExpObj = Object.create(StrExp.prototype);
-        const modifiedArray = [...iteratable];
+    /**
+     * 反復可能オブジェクトをStrExpに変換
+     * @param {Iterable} iterable 反復可能オブジェクト
+     * @returns StrExp
+     */
+    static from(iterable){
+        const StrExpObj = Object.create(this.prototype);
+        const modifiedArray = [...iterable];
         for(const propName in modifiedArray){
             StrExpObj[propName] = modifiedArray[propName]
         }
         StrExpObj.length = modifiedArray.length;
-        StrExpObj.EXP_ID = (function(){
+        StrExpObj[StrExp.getExpIdKey] = (function(){
             const ID_STR = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
             let id = '';
             outerLoop: for(let j = 0; j < 4; j++){
@@ -347,7 +424,9 @@ class StrExp{
             }
             return id;
         })();
+        return StrExpObj;
     }
+    static getExpIdKey = Symbol('expKey')
     /**
      * 代入
      * @param {object} assignNums 代入する文字と値をまとめたオブジェクト
@@ -359,7 +438,11 @@ class StrExp{
         for(const exp of this){
             result += (
                 exp.multiedNum * 
-                ((assignNums[exp.multiedStr] ?? defaultNum) ** exp.pow)
+                (((
+                    (exp.multiedStr.method !== 'factorial') ? 
+                    ((exp.multiedStr.method !== null) ?
+                    Math[exp.multiedStr.method] : factorial) : (n => n)
+                )(assignNums[exp.multiedStr.string]) ?? defaultNum) ** exp.pow)
             );
         }
         return result;
@@ -367,8 +450,9 @@ class StrExp{
     toString(){
         let returnStr = '';
         for (const str of this){
+            const m = str.multiedNum.method;
             if (str.multiedNum.toString().match('\\-')) {
-                returnStr += str.multiedNum + str.multiedStr;
+                returnStr += `${str.multiedNum}${m ?? ''}${m ? '(' : ''}${str.multiedStr.string}${m ? ')' : ''}`;
             } else {
                 returnStr += (
                     returnStr ? '+' : ''
@@ -380,13 +464,19 @@ class StrExp{
         }
         return returnStr;
     }
+    add(objExp){
+        return StrExp.positive(this, objExp);
+    }
+    sub(objExp){
+        return StrExp.negative(this, objExp);
+    }
+    mul(objExp){
+        return StrExp.multiply(this, objExp);
+    }
+    div(objExp){
+        return StrExp.divide(this, objExp);
+    }
     static version = 'Beta 2.2.5';
 }
 
 const SE = StrExp;
-
-Object.defineProperty(String.prototype, 'canConvertStrExp', {
-    value: function(){
-        return StrExp.PATTERN.test(this.valueOf());
-    }
-});
